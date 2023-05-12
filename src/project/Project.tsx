c9bc16d2026fb28d28b730cb2837d1bfd4de31b6
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-05-12 13:31:37 +0200

    part 2 :refactoring for buttons in Project

--- src/project/Project.tsx
+++ src/project/Project.tsx
@@ -5,12 +5,13 @@ import style from './Project.module.css'
 type ProjectPropsType = {
     title: string
     description: any
-    style:any
+    style: any
 }
 const Project = (props: ProjectPropsType) => {
     return (
         <div className={style.smallProject}>
-            <div className={style.buttonDiv} style={props.style}><Button name={"watch"}/></div>
+            <div className={style.image} style={props.style}>
+                <a className={style.viewBtn}>Watch</a></div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectTitle}>{props.title}</h3>
                 <span className={style.description}>{props.description}</span>
