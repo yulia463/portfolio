9ee0016c9f5fb9303aae3da4c499eaf8f64c4b29
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-05-15 21:15:18 +0200

    part 2 : refactoring

--- src/project/Projects.tsx
+++ src/project/Projects.tsx
@@ -4,27 +4,27 @@ import Project from "./Project";
 import avatar from "../assets/image/avatar.png";
 
 const Projects = () => {
-    const social ={
-        backgroundImage:`url(${avatar})`
+    const social = {
+        backgroundImage: `url(${avatar})`
     }
-    const todolist ={
-        backgroundImage:`url(${avatar})`
+    const todolist = {
+        backgroundImage: `url(${avatar})`
     }
     return (
-        <div className={style.project} >
+        <div className={style.project}>
             <div className={style.projectContainer}>
                 <div className={style.title}>
-                    <h2 > Projects</h2>
+                    <h2> Projects</h2>
                 </div>
 
                 <div className={style.smallContainer}>
                     <Project style={social}
-                        title={"название проекта"}
-                        description={"краткое описание"}
+                             title={"название проекта"}
+                             description={"краткое описание"}
                     />
                     <Project style={todolist}
-                        title={"название проекта"}
-                        description={"краткое описание"}
+                             title={"название проекта"}
+                             description={"краткое описание"}
                     />
                 </div>
             </div>
