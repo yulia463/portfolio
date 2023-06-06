415545e4f2620bc51254690035e79ea09a9d1ba3
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-06-06 17:06:31 +0200

    part 2 : add material ui

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,23 +2,42 @@ import React from "react";
 import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.scss';
 import Skill from "./skill/Skill";
-
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import CssIcon from '@mui/icons-material/Css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import {pink, yellow} from "@mui/material/colors";
+import {blue} from "@material-ui/core/colors";
 
 const Skills = () => {
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer} `}>
-               <div className={style.title}>
-                   <h2 > Skills</h2>
-               </div>
+                <div className={style.title}>
+                    <h2> Skills</h2>
+                </div>
                 <div className={style.skills}>
                     <Skill
+                        icon={<JavascriptIcon
+                            color="action"
+                            sx={{color: yellow[600]}}
+                            fontSize="large"
+                        />}
                         title={"JS"}
                         description={"Lorem ipsum dolor sit amet, consectetur asperiores atque ducimus error"}/>
                     <Skill
+                        icon={<CssIcon
+                            color="action"
+                            sx={{color: blue[800]}}
+                            fontSize="large"
+                        />}
                         title={"CSS"}
                         description={"excepturi, fuga hic mollitia nesciunt, perspiciatis quaerat quis quo"}/>
                     <Skill
+                        icon={<GitHubIcon
+                            sx={{color: blue[800]}}
+                            fontSize="large"
+                        />}
                         title={"REACT"}
                         description={"repudiandae rerumsapiente tempora!"}/>
                 </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import style from './Skill.module.scss';
 
+
 type SkillPropsType = {
     title: string
     description: any
+    icon: React.ReactNode
 }
 
 const Skill = (props: SkillPropsType) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon}>
+                {props.icon}
+            </div>
             <h3>{props.title}</h3>
             <span className={style.description}>
                 {props.description}
